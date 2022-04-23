97278cf9af016a06bb26736d54c03658a4424687
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-23 15:22:59 -0700

    Modified UserForm to create a new User data object once data is submitted and styled the component

--- user-management-app/src/components/AddUser/UserForm.js
+++ user-management-app/src/components/AddUser/UserForm.js
@@ -1,21 +1,69 @@
-// import classes from "./UserForm.module.css";
+import { useState } from "react";
 const UserForm = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
 
-    const submitFormHandler = (event) => {
-        event.preventDefault();
-    }
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      username: username,
+      age: age,
+    };
+
+    console.log(newUser);
+    setUsername("");
+    setAge("");
+  };
+
+  const inputUsernameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const inputAgeHandler = (event) => {
+    setAge(event.target.value);
+  };
 
   return (
-    <div>
-      <div>
-        <label htmlFor="usernam">Username</label>
-        <input type="text" name="username" id="username" />
-      </div>
-      <div>
-        <label htmlFor="age">Age (Years)</label>
-        <input type="text" name="age" id="age" />
-      </div>
-      <button type="submit">Add User</button>
+    <div className="card text-start">
+      <form action="" className="form">
+        <h2>Add User Form</h2>
+        <div className="row">
+          <div className="col-12 col-md-6 mb-2">
+            <label className="form-label" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="username"
+              id="username"
+              onInput={inputUsernameHandler}
+              value={username}
+            />
+          </div>
+          <div className="col-12 col-md-6 mb-2">
+            <label className="form-label" htmlFor="age">
+              Age (Years)
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="age"
+              id="age"
+              onInput={inputAgeHandler}
+              value={age}
+            />
+          </div>
+        </div>
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={submitFormHandler}
+        >
+          Add User
+        </button>
+      </form>
     </div>
   );
 };
