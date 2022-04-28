b30a435df86f5be780ff261d9cd86756c4dfbf24
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-27 19:36:50 -0700

    Modified code to only show UserList if there are users in the program

--- user-management-app/src/components/UserList.js
+++ user-management-app/src/components/UserList.js
@@ -1,17 +1,22 @@
 import User from "./User";
 import Card from "./UI/Card";
 const UserList = (props) => {
-  return (
-    <Card>
-      <ul>
-        {props.users.map((user) => (
-          <li key={user.id}>
-            <User username={user.username} age={user.age}  />
-          </li>
-        ))}
-      </ul>
-    </Card>
-  );
+  console.log(props.users.length);
+  if (props.users.length > 0) {
+    return (
+      <Card>
+        <ul>
+          {props.users.map((user) => (
+            <li key={user.id}>
+              <User username={user.username} age={user.age} />
+            </li>
+          ))}
+        </ul>
+      </Card>
+    );
+  } else {
+    return;
+  }
 };
 
 export default UserList;
