427c8c4f4c52d4b2f6da5fd70d4d59d1c238d879
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-27 19:19:17 -0700

    Modified Card component to allow me to pass other classes to it via className prop

--- user-management-app/src/components/UI/Card.js
+++ user-management-app/src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = (props) => {
-  return <div className="card my-4 p-2">{props.children}</div>;
+  return <div className={`card my-4 p-2 ${props.className}`}>{props.children}</div>;
 };
 
 export default Card;
