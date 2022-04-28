36334c3b3bbd7cda89bfbeddd5b377d6a8663d4e
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-27 20:10:10 -0700

    Made some UI changes to the UserList

--- user-management-app/src/components/UserList.js
+++ user-management-app/src/components/UserList.js
@@ -1,17 +1,14 @@
 import User from "./User";
 import Card from "./UI/Card";
 const UserList = (props) => {
-  console.log(props.users.length);
   if (props.users.length > 0) {
     return (
-      <Card>
-        <ul>
-          {props.users.map((user) => (
-            <li key={user.id}>
-              <User username={user.username} age={user.age} />
-            </li>
-          ))}
-        </ul>
+      <Card className="bg-white">
+        {props.users.map((user) => (
+          <div key={user.id}>
+            <User username={user.username} age={user.age} />
+          </div>
+        ))}
       </Card>
     );
   } else {
