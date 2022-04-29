9dbc6e9ccb710c8f87119b568b017fbab7b1989e
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-28 21:34:22 -0700

    Added a new Wrapper component to prevent unnecessary usage of "root divs" for JSX code

---
+++ user-management-app/src/components/Helper/Wrapper.js
@@ -0,0 +1,6 @@
+// This component is meant to be used as a general wrapping component to prevent unnecessary usage of nested divs as wrapping components
+const Wrapper = (props) =>  {
+    return props.children;
+}
+
+export default Wrapper;
